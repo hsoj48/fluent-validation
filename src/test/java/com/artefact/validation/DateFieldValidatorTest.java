59d1f5f03837463116f825508d20e63866bcaf52
Josh Fields <Josh.Fields@CENTENE.COM>
2020-09-01 19:58:22 -0500

    Unit tests

---
+++ src/test/java/com/artefact/validation/DateFieldValidatorTest.java
@@ -0,0 +1,75 @@
+package com.artefact.validation;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DateFieldValidatorTest {
+
+    ValidationHandler sut_ValidationHandler;
+
+    @Before
+    public void init() {
+        sut_ValidationHandler = new DefaultValidationHandler();
+    }
+
+    @Test
+    public void testDate_BeforeMethods_HappyPath() {
+        sut_ValidationHandler.requireThat("testDate", new Date(0))
+                .isBeforeNow()
+                .isBefore(new Date(1));
+
+        assertFalse(sut_ValidationHandler.hasErrors());
+    }
+
+    @Test
+    public void testDate_AfterMethods_HappyPath() {
+        sut_ValidationHandler.requireThat("testDate", new Date(System.currentTimeMillis() + 10000))
+                .isAfterNow()
+                .isAfter(new Date());
+
+        assertFalse(sut_ValidationHandler.hasErrors());
+    }
+
+    @Test
+    public void testDate_AllNull_ProducesSafeErrors() {
+        sut_ValidationHandler.requireThat("testDate", (Date) null)
+                .isBeforeNow()
+                .isBefore(new Date(1))
+                .isAfterNow()
+                .isAfter(new Date());
+
+        assertFalse(sut_ValidationHandler.hasErrors());
+    }
+
+    @Test
+    public void testDate_AllFailures_ProducesSafeErrors() {
+        sut_ValidationHandler = new DefaultValidationHandler();
+        sut_ValidationHandler.requireThat("testDate", new Date(System.currentTimeMillis() + 10000)).isBeforeNow();
+        assertTrue(sut_ValidationHandler.hasErrors());
+        assertEquals("Invalid testDate", sut_ValidationHandler.getErrorMessagesJoined());
+
+        sut_ValidationHandler = new DefaultValidationHandler();
+        sut_ValidationHandler.requireThat("testDate", new Date(System.currentTimeMillis() + 10000)).isBefore(new Date(1));
+        assertTrue(sut_ValidationHandler.hasErrors());
+        assertEquals("Invalid testDate", sut_ValidationHandler.getErrorMessagesJoined());
+
+        sut_ValidationHandler = new DefaultValidationHandler();
+        sut_ValidationHandler.requireThat("testDate", new Date(0)).isAfterNow();
+        assertTrue(sut_ValidationHandler.hasErrors());
+        assertEquals("Invalid testDate", sut_ValidationHandler.getErrorMessagesJoined());
+
+        sut_ValidationHandler = new DefaultValidationHandler();
+
+        sut_ValidationHandler.requireThat("testDate", new Date(0)).isAfter(new Date());
+        assertTrue(sut_ValidationHandler.hasErrors());
+        assertEquals("Invalid testDate", sut_ValidationHandler.getErrorMessagesJoined());
+    }
+
+}
---
+++ src/test/java/com/artefact/validation/GenericFieldValidatorTest.java
@@ -0,0 +1,113 @@
+package com.artefact.validation;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GenericFieldValidatorTest {
+
+    ValidationHandler sut_ValidationHandler;
+
+    @Before
+    public void init() {
+        sut_ValidationHandler = new DefaultValidationHandler();
+    }
+
+    @Test
+    public void testInteger_HappyPath() {
+        sut_ValidationHandler.requireThat("testInteger", 123)
+                .isPresent()
+                .matches(123)
+                .matchesOneOf(123, 456)
+                .matchesNoneOf(111, 222, null)
+                .isTrue(true)
+                .isTrue(o -> true)
+                .isFalse(false)
+                .isFalse(o -> false);
+
+        assertFalse(sut_ValidationHandler.hasErrors());
+    }
+
+    @Test
+    public void testInteger_AllNull_ProducesSafeErrors() {
+        sut_ValidationHandler = new DefaultValidationHandler();
+        sut_ValidationHandler.requireThat("testInteger", (Integer) null).isPresent();
+        assertTrue(sut_ValidationHandler.hasErrors());
+
+        sut_ValidationHandler = new DefaultValidationHandler();
+        sut_ValidationHandler.requireThat("testInteger", (Integer) null).matches(123);
+        assertFalse(sut_ValidationHandler.hasErrors());
+
+        sut_ValidationHandler = new DefaultValidationHandler();
+        sut_ValidationHandler.requireThat("testInteger", (Integer) null).matchesOneOf(123, 456);
+        assertFalse(sut_ValidationHandler.hasErrors());
+
+        sut_ValidationHandler = new DefaultValidationHandler();
+        sut_ValidationHandler.requireThat("testInteger", (Integer) null).matchesNoneOf(111, 222, null);
+        assertFalse(sut_ValidationHandler.hasErrors());
+
+        sut_ValidationHandler = new DefaultValidationHandler();
+        sut_ValidationHandler.requireThat("testInteger", (Integer) null).isTrue(true);
+        assertFalse(sut_ValidationHandler.hasErrors());
+
+        sut_ValidationHandler = new DefaultValidationHandler();
+        sut_ValidationHandler.requireThat("testInteger", (Integer) null).isTrue(o -> true);
+        assertFalse(sut_ValidationHandler.hasErrors());
+
+        sut_ValidationHandler = new DefaultValidationHandler();
+        sut_ValidationHandler.requireThat("testInteger", (Integer) null).isFalse(false);
+        assertFalse(sut_ValidationHandler.hasErrors());
+
+        sut_ValidationHandler = new DefaultValidationHandler();
+        sut_ValidationHandler.requireThat("testInteger", (Integer) null).isFalse(o -> false);
+        assertFalse(sut_ValidationHandler.hasErrors());
+    }
+
+    @Test
+    public void testInteger_AllFailures_ProducesSafeErrors() {
+        sut_ValidationHandler = new DefaultValidationHandler();
+        sut_ValidationHandler.requireThat("testInteger", (Integer) null).isPresent();
+        assertTrue(sut_ValidationHandler.hasErrors());
+        assertEquals("Invalid testInteger", sut_ValidationHandler.getErrorMessagesJoined());
+
+        sut_ValidationHandler = new DefaultValidationHandler();
+        sut_ValidationHandler.requireThat("testInteger", 234).matches(123);
+        assertTrue(sut_ValidationHandler.hasErrors());
+        assertEquals("Invalid testInteger", sut_ValidationHandler.getErrorMessagesJoined());
+
+        sut_ValidationHandler = new DefaultValidationHandler();
+        sut_ValidationHandler.requireThat("testInteger", 234).matchesOneOf(123, 456);
+        assertTrue(sut_ValidationHandler.hasErrors());
+        assertEquals("Invalid testInteger", sut_ValidationHandler.getErrorMessagesJoined());
+
+        sut_ValidationHandler = new DefaultValidationHandler();
+        sut_ValidationHandler.requireThat("testInteger", 234).matchesNoneOf(111, 234, null);
+        assertTrue(sut_ValidationHandler.hasErrors());
+        assertEquals("Invalid testInteger", sut_ValidationHandler.getErrorMessagesJoined());
+
+        sut_ValidationHandler = new DefaultValidationHandler();
+        sut_ValidationHandler.requireThat("testInteger", 234).isTrue(false);
+        assertTrue(sut_ValidationHandler.hasErrors());
+        assertEquals("Invalid testInteger", sut_ValidationHandler.getErrorMessagesJoined());
+
+        sut_ValidationHandler = new DefaultValidationHandler();
+        sut_ValidationHandler.requireThat("testInteger", 234).isTrue(o -> false);
+        assertTrue(sut_ValidationHandler.hasErrors());
+        assertEquals("Invalid testInteger", sut_ValidationHandler.getErrorMessagesJoined());
+
+        sut_ValidationHandler = new DefaultValidationHandler();
+        sut_ValidationHandler.requireThat("testInteger", 234).isFalse(true);
+        assertTrue(sut_ValidationHandler.hasErrors());
+        assertEquals("Invalid testInteger", sut_ValidationHandler.getErrorMessagesJoined());
+
+        sut_ValidationHandler = new DefaultValidationHandler();
+        sut_ValidationHandler.requireThat("testInteger", 234).isFalse(o -> true);
+        assertTrue(sut_ValidationHandler.hasErrors());
+        assertEquals("Invalid testInteger", sut_ValidationHandler.getErrorMessagesJoined());
+    }
+
+}
